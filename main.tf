c0461ac931c088116483f7433946c6a02bbb90d4
KHAN ABDULLAH ADIL <36574088+khann-adill@users.noreply.github.com>
2021-08-11 18:35:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
   one_nat_gateway_per_az = false 
-  #azs                = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  #public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  #private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  #tags = {
-  #  Terraform   = "true"
-  #  Environment = "dev"
-  #}
+  azs                = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
