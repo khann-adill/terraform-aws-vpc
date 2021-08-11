f0dd5e20bf7a5e5251d2cd0c65cedae1dab93703
KHAN ABDULLAH ADIL <36574088+khann-adill@users.noreply.github.com>
2021-08-11 18:32:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ provider "aws" {
 
 module "vpc" {
   source             = "./modules/vpc"
-  name               = "my-vpc"
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  one_nat_gateway_per_az = false 
-  azs                = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  #name               = "my-vpc"
+  #enable_nat_gateway = true
+  #single_nat_gateway = true
+  #one_nat_gateway_per_az = false 
+  #azs                = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  #public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  #private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  #tags = {
+  #  Terraform   = "true"
+  #  Environment = "dev"
+  #}
 }
