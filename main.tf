50955b5f5a5c936aa62e2a7e83ffd0482a68e32c
root <root@ip-172-31-14-195.us-east-2.compute.internal>
2021-08-19 07:36:46 +0000

    added outputs for vpc module

--- main.tf
+++ main.tf
@@ -8,6 +8,33 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
+provider "aws" {
+  region  = "us-east-2"
+  profile = "admin"
+}
 module "vpc" {
-  source = "./modules/vpc"
+  source                 = "./module/vpc"
+  name                   = "rds-vpc"
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+  azs                    = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets         = ["10.0.1.0/24"]
+  private_subnets        = ["10.0.2.0/24", "10.0.3.0/24"]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    RDS         = "mysql-dev"
+  }
+}
+
+output "private_subnet"{
+    value = module.vpc.private_subnet
+}
+
+output "vpc_id"{
+    value = module.vpc.vpc_id
+}
+output "public_subnet"{
+    value = module.vpc.public_subnet
 }
---
+++ module/vpc/outputs.tf
@@ -0,0 +1,14 @@
+output "private_subnet" {
+  value       = [aws_subnet.private.*.id]
+  description = "List of private subnets"
+}
+
+output "public_subnet" {
+  value       = [aws_subnet.public.*.id]
+  description = "List of public subnets"
+}
+
+output "vpc_id" {
+  value       = local.vpc_id
+  description = "VPC ID"
+}
