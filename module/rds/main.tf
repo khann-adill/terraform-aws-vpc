7647de760e8e7c4ca7e2fb8b4b837f1d5108c7f2
root <root@ip-172-31-14-195.us-east-2.compute.internal>
2021-08-20 10:39:05 +0000

    change code for rds

--- module/rds/main.tf
+++ module/rds/main.tf
@@ -42,57 +42,57 @@ module "vpc" {
 #}
 
 resource "aws_db_subnet_group" "that" {
-	name = "rds-testing"
-	subnet_ids = module.vpc.private_subnet
+  name       = "rds-testing"
+  subnet_ids = module.vpc.private_subnet
 
-	tags = {
-	Name = "rds-testing"
-	}
+  tags = {
+    Name = "rds-testing"
+  }
 }
 
 resource "aws_security_group" "that" {
-        name = "db-sg"
-        vpc_id = module.vpc.vpc_id
+  name   = "db-sg"
+  vpc_id = module.vpc.vpc_id
 
-        ingress {
-          from_port = 3306
-	  to_port = 3306
-	  protocol = "tcp"
-	  cidr_blocks = ["0.0.0.0/0"]
-	}
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-	egress {
-         from_port   = 3306
-         to_port     = 3306
-         protocol    = "tcp"
-         cidr_blocks = ["0.0.0.0/0"]
-        }
-        tags = {
-         Name = "db-sg"
-        }
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "db-sg"
+  }
 }
 
 resource "aws_security_group" "this" {
-        name = "public-sg"
-        vpc_id = module.vpc.vpc_id
+  name   = "public-sg"
+  vpc_id = module.vpc.vpc_id
 
-        ingress {
-          from_port = 22
-          to_port = 22
-          protocol = "tcp"
-          description = "ssh access"
-          cidr_blocks = ["0.0.0.0/0"]
-         }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    description = "ssh access"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        egress {
-         from_port   = 0
-         to_port     = 0
-         protocol    = "-1"
-         cidr_blocks = ["0.0.0.0/0"]
-        }
-        tags = {
-         Name = "public-sg"
-        }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "public-sg"
+  }
 }
 
 variable "db_password" {
