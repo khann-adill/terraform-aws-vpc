c25129dec087d446c92622c1b4718d396d657e41
KHAN ABDULLAH ADIL <36574088+khann-adill@users.noreply.github.com>
2021-09-14 13:28:53 +0530

    Update private_routing-tables.tf

--- module/vpc/private_routing-tables.tf
+++ module/vpc/private_routing-tables.tf
@@ -1,5 +1,4 @@
 resource "aws_route_table" "Private-Subnet-RT" {
-  #count  = var.create_vpc && length(var.private_subnets) > 0 && var.one_nat_gateway_per_az ? length(var.private_subnets) : 1
   count  = var.create_vpc && length(var.private_subnets) = 0 ? 0 : var.one_nat_gateway_per_az ? length(var.private_subnets) : 1
   vpc_id = local.vpc_id
   route {
